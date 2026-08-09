c9777996d537a0f578989b0c461aac095cbe03ef
Djibril Mugisho <mugishodjibril7@gmail.com>
2026-08-09 23:19:33 +0300

    Update functions.ts

--- app/functions.ts
+++ app/functions.ts
@@ -2,6 +2,8 @@ import * as path from "path";
 import * as fs from "fs";
 import { execSync } from "child_process";
 
+
+
 export function readFile({ file_path }: { file_path: string }) {
   const absolutePath = path.resolve(process.cwd(), file_path);
   const content = fs.readFileSync(absolutePath, "utf-8");
